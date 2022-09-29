257f02ff60d83f3650246bf4bb21c35dfb6ba390
Juggernauff <66491707+Juggernauff@users.noreply.github.com>
2022-09-29 14:36:37 +0400

    Added Task2.

---
+++ RITG/Task2/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RITG.Task2
+{
+    internal class Person : IComparable<Person>
+    {
+        public Person(string lastName, string firstName, string middleName)
+        {
+            Console.WriteLine($"Вызов конструктора {nameof(Person)}");
+            Id = ++NumberPeople;
+            LastName = lastName;
+            FirstName = firstName;
+            MiddleName = middleName;
+        }
+        static Person()
+        {
+            Console.WriteLine($"Вызов статичного конструктора {nameof(Person)}");
+            NumberPeople = 0;
+        }
+        public static int NumberPeople { get; private set; }
+        public int Id { get; }
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string MiddleName { get; }
+
+        public int CompareTo(Person other) =>
+            Id.CompareTo(other.Id);
+
+    }
+}
---
+++ RITG/Task2/Sorting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RITG.Task2
+{
+    enum SortOrder
+    {
+        Ascending,
+        Descending
+    }
+    internal static class Sorting
+    {
+        public static void Swap<T>(ref T value1, ref T value2) =>
+            (value1, value2) = (value2, value1);
+        public static bool CheckSortOrder<T>(this T[] array, SortOrder sortOrder = SortOrder.Ascending)
+            where T : IComparable<T>
+        {
+            if (array is null)
+                throw new ArgumentNullException(nameof(array));
+            switch(sortOrder)
+            {
+                case SortOrder.Ascending:
+                    for (var i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i].Equals(null))
+                            continue;
+                        if (array[i].CompareTo(array[i + 1]) > 0)
+                            return false;
+                    }
+                    break;
+                case SortOrder.Descending:
+                    for (var i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i].Equals(null))
+                            continue;
+                        if (array[i].CompareTo(array[i + 1]) < 0)
+                            return false;
+                    }
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+            return true;
+        }
+    }
+}
