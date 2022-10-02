1d4e7d6ecb6d18524572e4d96797db98c5c75f60
Danila Khodarev <66491707+Juggernauff@users.noreply.github.com>
2022-10-03 00:22:17 +0300

    Added Task6.

---
+++ RITG/Task6/Task6.sql
@@ -0,0 +1,12 @@
+﻿SELECT *
+FROM Users U1
+JOIN Profile P
+ON U1.Id = P.UserId
+JOIN TmpProfile TP
+ON U1.Login = TP.Login
+WHERE U1.Age IN (
+SELECT MAX(U.Age) AS 'MaxAge'
+FROM Users U
+JOIN TmpProfile TP
+ON U.Login = TP.Login
+GROUP BY TP.FirmId)
