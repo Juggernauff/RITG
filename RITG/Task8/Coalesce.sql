fb17d2443e6b58d86564b104faf132dbc16dcd75
Danila Khodarev <66491707+Juggernauff@users.noreply.github.com>
2022-10-02 18:13:23 +0300

    Added Task8.

---
+++ RITG/Task8/Coalesce.sql
@@ -0,0 +1,9 @@
+﻿SELECT U.Id, COALESCE(U.LastLogin, 'Never') AS 'LastLogin'
+FROM [Users] U
+
+SELECT P.Id, COALESCE(P.FIO, 'NoName') AS 'FullName'
+FROM [Profile] P
+
+SELECT COALESCE ((SELECT COUNT(*)
+FROM [Profile] P
+WHERE P.UserId IN (SELECT U.Id FROM Users U)), 0) AS 'Number of users with a profile'
